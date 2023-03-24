1b4475d0b2a1f76c07da9bd4b10d7073ca2e1500
Broh7020 <braudo190@gmail.com>
2023-03-24 06:52:37 -0700

    Numbers constitute

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,17 +1,17 @@
 #include "main.h"
 
 /**
- * print_numbers - prints 0 to 9.
+ * print_numbers - checks for checks for a digit (0 through 9).
  *
+ * Return: Always 0.
  */
 void print_numbers(void)
 {
-	int num = 0;
+ int c;
 
-	while (num < 10)
-	{
-		_putchar('0' + num);
-		num++;
-	}
-	_putchar('\n');
+ for (c = 48; c < 58; c++)
+ {
+ _putchar(c);
+ }
+ _putchar('\n');
 }
--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,24 +1,24 @@
 #include "main.h"
 
 /**
- * more_numbers - a function that prints 10 times the numbers, from 0 to 14
- * _putchar only 3 times
- * Return: 0-14 x10 followed by new line
+ * more_numbers - prints 0 to 14 on one line ten times.
+ *
  */
 void more_numbers(void)
 {
-	int i, ro;
+	int i;
+	int num = 0;
 
-	for (ro = 0; ro < 10; ro++)
+	for (i = 0 ; i < 10 ; i++)
 	{
-		for (i = 0; i <= 14; i++)
+		while (num < 15)
 		{
-			if (i >= 10)
-			{
-				_putchar((i / 10) + '0');
-			}
-			_putchar((i % 10) + '0');
+			if (num > 9)
+				_putchar('0' + num / 10);
+			_putchar('0' + num % 10);
+			num++;
 		}
+		num = 0;
 		_putchar('\n');
 	}
 }
