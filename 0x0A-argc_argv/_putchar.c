3dcf5b7898b435b21d06efd9c1c746b18b2b0b03
Broh7020 <braudo190@gmail.com>
2023-04-20 00:55:36 -0700

    main.h

--- 0x0A-argc_argv/_putchar.c
+++ 0x0A-argc_argv/_putchar.c
@@ -1,4 +1,4 @@
-#include <unistd.h>
+#include <stdio.h>
 #include "main.h"
 
 /**
@@ -8,6 +8,7 @@
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
+
 int _putchar(char c)
 {
 return (write(1,&c, 1));
